2e93382796047622130edf0fa6a9562186b140c0
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-09-17 16:44:55 +0900

    D

--- main.c
+++ main.c
@@ -5,18 +5,18 @@
 
 int main(int argc, char *argv[]) {
     int a;
-    int b;
+    float b;
     float i;
 	
 	printf(" 분자를 입력하세요 : " );
-	scanf("%a", &a);
+	scanf("%i", &a);
 	printf(" 분모를 입력하세요 : " );
-	scanf("%b", &b);
+	scanf("%f", &b);
 	
 	
 	  
-	i = b/a;	
-   printf(" 나누기의 결과는 %i 입니다. \n", i);
+	i = a/b;	
+   printf(" 나누기의 결과는 %f 입니다. \n", i);
      
 	return 0;
 }
